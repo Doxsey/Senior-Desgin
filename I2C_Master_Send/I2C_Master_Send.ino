848b8a27f0eb7988fc667c4dc2a0437288cc1b86
Doxsey <ryan.doxsey@gmail.com>
2018-04-27 14:52:53 -0500

    Data packet shaping and standard i2c send

--- I2C_Master_Send/I2C_Master_Send.ino
+++ I2C_Master_Send/I2C_Master_Send.ino
@@ -1,25 +1,39 @@
-byte bit0 = B01;
-byte bit1 = B10;
-byte bit2 = B00;
-byte I2C_Addr = 0;
+#define I2C_Low 0b00000000 //00 denotes to set the output pin to low
+#define I2C_High 0b00000011 //11 denotes to set the output pin to high
+#define I2C_Float 0b00000010 //10 denotes to set the pin to and input, causing it to be a floating voltage
 
-void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600); 
-bitWrite(I2C_Addr, 5, bitRead(bit0, 1));
-bitWrite(I2C_Addr, 4, bitRead(bit0, 0));
 
-bitWrite(I2C_Addr, 3, bitRead(bit1, 1));
-bitWrite(I2C_Addr, 2, bitRead(bit1, 0));
+byte CA2 = I2C_High; //Set the CA2 bit address here (Address 100 is reserved for the ATMega IC)
+byte CA1 = I2C_Low;
+byte CA0 = I2C_Float;
 
-bitWrite(I2C_Addr, 1, bitRead(bit2, 1));
-bitWrite(I2C_Addr, 0, bitRead(bit2, 0));
+byte I2C_Data = 0; //This is the data packet to send to the I2C slave.
+byte temp_Data = 0;
+
+//byte aVar = 0b10100101;  //decimal 10 in msb and decimal 5 in lsb
+void setup() 
+{
+  //Serial.begin(9600);
+  I2C_Data = (I2C_Data | CA2) << 2;
+  I2C_Data = (I2C_Data | CA1) << 2;
+  I2C_Data = (I2C_Data | CA0);
+  
+  Wire.begin(); // join i2c bus
+  
+  Wire.beginTransmission(100); // transmit to device #100 (0x64)
+                              // device address is specified in datasheet
+  Wire.write(I2C_Data);       // sends I2C_Data byte  
+  Wire.endTransmission();     // stop transmitting
+
+  
+  //int msb = (aVar & 0b11110000) >> 4;  //get the first 4 bits of the byte and shift them right 4 places
+  //int lsb = aVar & 0b00001111;         //get the last 4 bits of the byte
+  //Serial.println(I2C_Data);
+  //Serial.println(CA2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-//Serial.println(bit0 | I2C_Addr);
-//I2C_Addr = I2C_Addr << 2;
-Serial.println(I2C_Addr);
-while(1);
+void loop() 
+{
+
 }
+
