2959fb476af35d04f39c14c06c901ca84ebb1b30
Doxsey <ryan.doxsey@gmail.com>
2018-04-29 10:34:44 -0500

    Test Parsing of I2C data packet

--- I2C_Slave/I2C_Slave.ino
+++ I2C_Slave/I2C_Slave.ino
@@ -1,20 +1,10 @@
-// Wire Slave Sender
-// by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Sends data as an I2C/TWI slave device
-// Refer to the "Wire Master Reader" example for use with this
-
-// Created 29 March 2006
-
-// This example code is in the public domain.
-
-
 #include <Wire.h>
 
-byte CA2 = 0;
-byte CA1 = 0;
-byte CA0 = 0;
+int CA2 = 0;
+int CA1 = 0;
+int CA0 = 0;
+
+
 
 void I2C_Action(int);
 
@@ -26,7 +16,7 @@ void setup() {
   pinMode(9, OUTPUT);
   pinMode(8, OUTPUT);
 
-  digitalWrite(9, HIGH);
+  
 }
 
 void loop() {
@@ -37,20 +27,70 @@ void I2C_Action(int nBytes)
 {
   digitalWrite(8, HIGH);
   byte I2C_Data = Wire.read();
+  
+  CA2 = (I2C_Data & 110000) >> 4;
+  CA1 = (I2C_Data & 001100) >> 2;
+  CA0 = (I2C_Data & 11);
+  
   Serial.print("I2C Data Packet: ");
   Serial.println(I2C_Data, BIN);
+  
+switch (CA2) {
+	case 0:
+		Serial.print("CA2: ");
+		Serial.println("LOW");
+		break;
+	case 2:
+		Serial.print("CA2: ");
+		Serial.println("FLOAT");
+		break;
+	case 3:
+		Serial.print("CA2: ");
+		Serial.println("HIGH");
+		break;
+	default:
+		break;
+}
+  
+switch (CA1) {
+	case 0:
+		Serial.print("CA1: ");
+		Serial.println("LOW");
+		break;
+	case 2:
+		Serial.print("CA1: ");
+		Serial.println("FLOAT");
+		break;
+	case 3:
+		Serial.print("CA1: ");
+		Serial.println("HIGH");
+		
+		break;
+	default:
+		break;
+	
+	}  
 
-  CA2 = (I2C_Data & 110000) >> 4;
-  Serial.print("CA2: ");
-  Serial.println(CA2, BIN);
+switch (CA0) {
+	case 0:
+		Serial.print("CA0: ");
+		Serial.println("LOW");
+		break;
+	case 2:
+		Serial.print("CA0: ");
+		Serial.println("FLOAT");
+		break;
+	case 3:
+		Serial.print("CA0: ");
+		Serial.println("HIGH");
+		
+		break;
+	default:
+		break;
+	
+	} 	
+  
 
-  CA1 = (I2C_Data & 001100) >> 2;
-  Serial.print("CA1: ");
-  Serial.println(CA1, BIN);
-
-  CA0 = (I2C_Data & 11);
-  Serial.print("CA0: ");
-  Serial.println(CA0, BIN);
   
   while(Wire.available()) //Get rest of data to clear the read buffer. We don't care about the rest of the data
   {
