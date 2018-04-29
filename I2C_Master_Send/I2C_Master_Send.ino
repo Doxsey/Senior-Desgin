307438ac338ae6cad20a839342783f9c4176169f
Doxsey <ryan.doxsey@gmail.com>
2018-04-29 12:42:02 -0500

    Simple Address Changer via I2C Working

--- I2C_Master_Send/I2C_Master_Send.ino
+++ I2C_Master_Send/I2C_Master_Send.ino
@@ -7,45 +7,27 @@
 
 byte CA2 = I2C_High; //Set the CA2 bit address here (Address 100 is reserved for the ATMega IC)
 byte CA1 = I2C_High;
-byte CA0 = I2C_High;
+byte CA0 = I2C_Low;
 
 byte I2C_Data = 0; //This is the data packet to send to the I2C slave.
 
-//byte aVar = 0b10100101;  //decimal 10 in msb and decimal 5 in lsb
 void setup() 
 {
-  //Serial.begin(9600);
-  I2C_Data = (I2C_Data | CA2) << 2;
-  I2C_Data = (I2C_Data | CA1) << 2;
-  I2C_Data = (I2C_Data | CA0);
-  
-  Wire.begin(); // join i2c bus with address of 1
-  
-  Wire.beginTransmission(100); // transmit to device #100 (0x64)
-                              // device address is specified in datasheet
-  Wire.write(I2C_Data);       // sends I2C_Data byte  
-  //Wire.write("hello");
-  Wire.endTransmission();     // stop transmitting
+	//Serial.begin(9600);
+	I2C_Data = (I2C_Data | CA2) << 2;
+	I2C_Data = (I2C_Data | CA1) << 2;
+	I2C_Data = (I2C_Data | CA0);
 
-  
-  //int msb = (aVar & 0b11110000) >> 4;  //get the first 4 bits of the byte and shift them right 4 places
-  //int lsb = aVar & 0b00001111;         //get the last 4 bits of the byte
-  //Serial.println(I2C_Data);
-  //Serial.println(CA2);
+	Wire.begin(); // join i2c bus with address of 1
+
+	Wire.beginTransmission(100); // transmit to device #100 (0x64)
+	Wire.write(I2C_Data);       // sends I2C_Data byte  
+	Wire.endTransmission();     // stop transmitting
+ 
 }
 
 void loop() 
 {
-while(1);
 
-delay(1000);
-Wire.beginTransmission(8); // transmit to device #100 (0x64)
-                              // device address is specified in datasheet
-//Wire.write("hello");
- Wire.write(I2C_Data);       // sends I2C_Data byte  
-  Wire.endTransmission();     // stop transmitting
-  
-  
 }
-
 
